645c537bd1dad11db8cbb182e84148e57666f374
sauvegarde <sauvegarde@Gauthier-2.local>
2025-01-06 11:42:53 +0100

    modification des cartes

--- src/main/java/codingweek/models/Card.java
+++ src/main/java/codingweek/models/Card.java
@@ -1,5 +1,31 @@
 package codingweek.models;
 
-public interface Card {
-    
+public class Card {
+
+    // Attributes
+    private String word; // Path to the image of the card
+    private boolean isRevealed = false;
+    private String color; // Color of the card (blue/red/neutral/assassin)
+
+    // Constructor
+    public Card(String word, String color){
+        this.word = word;
+        this.color = color;
+    }
+
+    // Getter
+    public String getWord(){
+        return this.word;
+    }
+    public String getColor(){
+        return this.color;
+    }
+
+    // Setter
+    public void setWord(String word){
+        this.word = word;
+    }
+    public void setColor(String color){
+        this.color = color;
+    }
 }
