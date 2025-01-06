ef77daa02f2bc1d0a4bf2112e48cfdd313b6d37d
Divya <divya-roshni.pugo@telecomnancy.eu>
2025-01-06 15:47:26 +0100

    Modif des board controllers

--- src/main/java/codingweek/controllers/MenuBarController.java
+++ src/main/java/codingweek/controllers/MenuBarController.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 
-public class BarreMenuControleur {
+public class MenuBarController {
 
     @FXML
     private void Charger(ActionEvent event) {
