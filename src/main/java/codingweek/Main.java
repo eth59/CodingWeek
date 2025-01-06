dc2d4a7ff20fbe8a8d3e9155c5174358950b1f0d
Ethan.Quilliot <ethan.quilliot@telecomnancy.eu>
2025-01-06 14:37:14 +0100

    patch

--- src/main/java/codingweek/Main.java
+++ src/main/java/codingweek/Main.java
@@ -1,3 +1,5 @@
+package codingweek;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -5,8 +7,6 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
-
-package codingweek;
 
 
 
