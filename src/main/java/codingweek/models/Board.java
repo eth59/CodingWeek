076ae923c52a31ac87abcae2a94cd85a65088aba
Ethan.Quilliot <ethan.quilliot@telecomnancy.eu>
2025-01-06 11:10:33 +0100

    Board

--- src/main/java/codingweek/models/Board.java
+++ src/main/java/codingweek/models/Board.java
@@ -1,5 +1,31 @@
 package codingweek.models;
 
+import java.util.ArrayList;
+
 public class Board {
-    
+    private ArrayList<Card> cards; // Doit être de taille boardSize * boardSize (25 par défaut)
+    private Board instance;
+
+    private Board() {
+        this.cards = new ArrayList<Card>();
+    }
+
+    public Board getInstance() {
+        if (instance == null) {
+            instance = new Board();
+        }
+        return instance;
+    }
+
+    public ArrayList<Card> getCards() {
+        return cards;
+    }
+
+    public void setCards(ArrayList<Card> cards) {
+        this.cards = cards;
+    }
+    // Il y a pas forcément besoin de setCards et addCard, 
+    public void addCard(Card card) {
+        cards.add(card);
+    }
 }
