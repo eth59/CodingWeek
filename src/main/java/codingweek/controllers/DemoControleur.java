43734bcacc4f00118f849c3bf9acf6b7e91ff0f1
Divya <divya-roshni.pugo@telecomnancy.eu>
2025-01-06 11:48:37 +0100

    board.fxml avec son controller

--- src/main/java/codingweek/controllers/DemoControleur.java
+++ src/main/java/codingweek/controllers/DemoControleur.java
@@ -1,19 +1,12 @@
 package codingweek.controllers;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 
 public class DemoControleur {
 
     @FXML
-    private Label versionLabel;
-
-    @FXML
     public void initialize() {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-
-        versionLabel.setText("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        // Add any initialization logic for DemoControleur, if needed
+        System.out.println("DemoControleur initialized.");
     }
-
 }
