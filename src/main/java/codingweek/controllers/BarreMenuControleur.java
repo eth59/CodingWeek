91c1f664f81fcc126f1bcd220878fbc573321e59
martinbiche67@gmail.com <martin.biche@telecomnancy.eu>
2025-01-06 16:29:32 +0100

    Retour menu pour fenetre active

--- src/main/java/codingweek/controllers/BarreMenuControleur.java
+++ src/main/java/codingweek/controllers/BarreMenuControleur.java
@@ -2,8 +2,17 @@ package codingweek.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import codingweek.Main;
 
 public class BarreMenuControleur {
 
@@ -19,7 +28,17 @@ public class BarreMenuControleur {
 
     @FXML
     private void RetourAcceuil(ActionEvent event) {
-        showNotImplementedError("Retour Acceuil");
+        try {
+            // Fermer la fenêtre actuelle
+            Stage currentStage = (Stage) ((MenuItem) event.getSource()).getParentPopup().getOwnerWindow();
+            currentStage.close(); // Ferme la fenêtre actuelle
+
+            // Charger la scène du menu
+            Main mainApp = new Main();
+            mainApp.start(new Stage());  // Créer une nouvelle scène et l'afficher
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     // Méthode utilitaire pour afficher une erreur
