05973b3fab45115bc77e131573e3d649ca75942f
martinbiche67@gmail.com <martin.biche@telecomnancy.eu>
2025-01-06 13:36:00 +0100

    Menu Bar

---
+++ src/main/java/codingweek/controllers/BarreMenuControleur.java
@@ -0,0 +1,33 @@
+package codingweek.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
+public class BarreMenuControleur {
+
+    @FXML
+    private void Charger(ActionEvent event) {
+        showNotImplementedError("Charger");
+    }
+
+    @FXML
+    private void Sauvegarder(ActionEvent event) {
+        showNotImplementedError("Sauvegarder");
+    }
+
+    @FXML
+    private void RetourAcceuil(ActionEvent event) {
+        showNotImplementedError("Retour Acceuil");
+    }
+
+    // Méthode utilitaire pour afficher une erreur
+    private void showNotImplementedError(String actionName) {
+        Alert alert = new Alert(AlertType.ERROR);
+        alert.setTitle("Fonctionnalité non implémentée");
+        alert.setHeaderText("Action \"" + actionName + "\" non disponible");
+        alert.setContentText("Cette fonctionnalité n'a pas encore été implémentée.");
+        alert.showAndWait();
+    }
+}
