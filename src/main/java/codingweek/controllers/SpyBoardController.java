d510f3538cb7a88fa0efc2e88d90bdfed4327a25
Divya <divya-roshni.pugo@telecomnancy.eu>
2025-01-07 16:38:50 +0100

    Fonctionalite d'afficher la carte devoilee pour l'espion avec le observateur design pattern

--- src/main/java/codingweek/controllers/SpyBoardController.java
+++ src/main/java/codingweek/controllers/SpyBoardController.java
@@ -1,5 +1,6 @@
 package codingweek.controllers;
 
+import codingweek.Observer;
 import codingweek.models.Board;
 import codingweek.models.Card;
 import codingweek.models.Game;
@@ -10,7 +11,7 @@ import javafx.scene.layout.StackPane;
 
 import java.util.List;
 
-public class SpyBoardController {
+public class SpyBoardController implements Observer {
 
     @FXML
     private GridPane boardGrid;
@@ -18,9 +19,54 @@ public class SpyBoardController {
     private final Game game = Game.getInstance();
     private final Board board = game.getBoard();
 
+    @Override
+    public void reagir() {
+        updateRevealedTiles();
+    }
+    
+    
+    private void updateRevealedTiles() {
+        List<Card> cards = board.getCards();
+        int gridSize = game.getBoardSize();
+    
+        int cardIndex = 0;
+        for (int row = 0; row < gridSize; row++) {
+            for (int col = 0; col < gridSize; col++) {
+                Card card = cards.get(cardIndex++);
+                if (card.isRevealed()) {
+                    StackPane cardPane = (StackPane) getNodeByRowColumnIndex(row, col, boardGrid);
+    
+                    if (cardPane != null) {
+                        String backgroundColor = convertColorToCSS(card.getColor());
+                        cardPane.setStyle("-fx-border-color: black; -fx-background-color: " + backgroundColor + "; -fx-padding: 10;");
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    private StackPane getNodeByRowColumnIndex(int row, int col, GridPane gridPane) {
+        for (javafx.scene.Node node : gridPane.getChildren()) {
+            if (GridPane.getRowIndex(node) == row && GridPane.getColumnIndex(node) == col) {
+                return (StackPane) node;
+            }
+        }
+        return null;
+    }
+
+    
+    private String convertColorToCSS(String javafxColor) {
+        if (javafxColor.startsWith("0x")) {
+            return "#" + javafxColor.substring(2, 8); 
+        }
+        return javafxColor; 
+    }
+
     public void initialize() {
         int gridSize = game.getBoardSize();
         populateBoard(gridSize);
+        board.addCardObservers(this);
     }
 
     private void populateBoard(int gridSize) {
@@ -32,7 +78,6 @@ public class SpyBoardController {
                 StackPane cardPane = new StackPane();
                 Card card = cards.get(cardIndex++);
 
-                // Couleur par defaut des tiles pas revelees
                 cardPane.setStyle("-fx-border-color: black; -fx-background-color: lightgrey; -fx-padding: 10;");
                 cardPane.setPrefSize(100, 100);
 
@@ -43,9 +88,5 @@ public class SpyBoardController {
                 boardGrid.add(cardPane, col, row);
             }
         }
-    }
-
-    public void onTileChange(int row, int col) {
-        System.out.println("Tile at (" + row + ", " + col + ") has been changed.");
     }
 }
