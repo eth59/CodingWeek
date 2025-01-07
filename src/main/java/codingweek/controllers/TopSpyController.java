3c93ad37b0c46ce8f5e297ed661da39447fbd418
martinbiche67@gmail.com <martin.biche@telecomnancy.eu>
2025-01-07 14:59:41 +0100

    Ajout du texte clignotant et sa couleur

--- src/main/java/codingweek/controllers/TopSpyController.java
+++ src/main/java/codingweek/controllers/TopSpyController.java
@@ -5,6 +5,10 @@ import codingweek.models.Game;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.animation.Timeline;
+import javafx.scene.control.Label;
+import javafx.util.Duration;
+import javafx.animation.FadeTransition;
 
 public class TopSpyController implements Observer {
     @FXML
@@ -14,23 +18,38 @@ public class TopSpyController implements Observer {
     TextField clueField, numberField;    
 
     private Game game;
+    private FadeTransition fadeTransition;
 
     public void initialize() {
         game = Game.getInstance();
         game.ajouterObservateur(this);
+        fadeTransition = new FadeTransition(Duration.seconds(0.5), turnLabel);
+        fadeTransition.setFromValue(1.0); // Opacité maximale (visible)
+        fadeTransition.setToValue(0.0);   // Opacité minimale (invisible)
+        fadeTransition.setCycleCount(FadeTransition.INDEFINITE);   // Le texte disparaît puis réapparaît
+        fadeTransition.setAutoReverse(true); // Revenir à l'état initial (visible)
         reagir();
     }
 
     public void reagir() {
         if (game.isSpyTurn() && game.isBlueTurn()) {
             turnLabel.setText("C'est au tour de l'espion bleu");
+            turnLabel.setStyle("-fx-text-fill: blue;");
+            startFading();
         } else if (game.isSpyTurn() && !game.isBlueTurn()) {
             turnLabel.setText("C'est au tour de l'espion rouge");
+            turnLabel.setStyle("-fx-text-fill: red;");
+            startFading();
         } else if (!game.isSpyTurn() && game.isBlueTurn()) {
             turnLabel.setText("C'est au tour du devin bleu");
+            turnLabel.setStyle("-fx-text-fill: blue;");
+            stopFading();
         } else {
             turnLabel.setText("C'est au tour du devin rouge");
+            turnLabel.setStyle("-fx-text-fill: red;");
+            stopFading();
         }
+        
     }
 
     public void submitClue() {
@@ -46,6 +65,18 @@ public class TopSpyController implements Observer {
             // Gérer l'erreur de conversion en integer
             numberField.clear();
             numberField.setPromptText("Veuillez entrer un nombre valide");
+        }
+    }
+
+    private void startFading() {
+        fadeTransition.play();
+    }
+
+    // Méthode pour arrêter l'animation de fondu
+    private void stopFading() {
+        if (fadeTransition != null) {
+            fadeTransition.stop();  // Arrêter l'animation
+            turnLabel.setOpacity(1.0);  // Assurer que le label reste visible
         }
     }
 }
